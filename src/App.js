c7e69319b2c3f348cdfdfeaa1457e884ac15c5e7
Max16 <max16@naver.com>
2021-04-28 16:34:22 +0900

    #3.0 Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,72 +1,25 @@
+import React from "react";
 import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Chicken",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.lLYnf87IJXPZqFVYyyCZDgHaDt%26pid%3DApi&f=1",
-    rating: 3.4,
-  },
-  {
-    id: 2,
-    name: "beef",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.8SryKbw2Tuwe_9c6duQMMAHaFj%26pid%3DApi&f=1",
-    rating: 4.5,
-  },
-  {
-    id: 3,
-    name: "pork",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.gTnpc75ixoT-dvcurE_YsQHaD-%26pid%3DApi&f=1",
-    rating: 4.0,
-  },
-  {
-    id: 4,
-    name: "rice",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.7KY5PG2_E9mlPN2OcxcmGgHaE7%26pid%3DApi&f=1",
-    rating: 3.2,
-  },
-  {
-    id: 5,
-    name: "salad",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.zbWkvlZszy3IHNiWljljuAHaKt%26pid%3DApi&f=1",
-    rating: 5.0,
-  },
-];
-
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h3>I like {name}</h3>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
-
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
