8d0b4d9a9bba8d560935da43f5859e9f99be5621
Max16 <max16@naver.com>
2021-04-28 15:15:54 +0900

    #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-function Food({ fav }) {
-  return <h3>I like {fav}</h3>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h3>I like {name}</h3>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "Chicken",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.lLYnf87IJXPZqFVYyyCZDgHaDt%26pid%3DApi&f=1",
+  },
+  {
+    name: "beef",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.8SryKbw2Tuwe_9c6duQMMAHaFj%26pid%3DApi&f=1",
+  },
+  {
+    name: "pork",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.gTnpc75ixoT-dvcurE_YsQHaD-%26pid%3DApi&f=1",
+  },
+  {
+    name: "rice",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.7KY5PG2_E9mlPN2OcxcmGgHaE7%26pid%3DApi&f=1",
+  },
+  {
+    name: "salad",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.zbWkvlZszy3IHNiWljljuAHaKt%26pid%3DApi&f=1",
+  },
+];
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="chicken" />
-      <Food fav="ramen" />
-      <Food fav="pork" />
-      <Food fav="beef" />
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
