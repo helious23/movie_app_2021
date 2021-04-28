9d508e111dd8e0491fed8715bd6f7f6de1038a05
Max16 <max16@naver.com>
2021-04-28 15:26:51 +0900

    #2.3 map Recap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,36 @@
+const foodILike = [
+  {
+    id: 1,
+    name: "Chicken",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.lLYnf87IJXPZqFVYyyCZDgHaDt%26pid%3DApi&f=1",
+  },
+  {
+    id: 2,
+    name: "beef",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.8SryKbw2Tuwe_9c6duQMMAHaFj%26pid%3DApi&f=1",
+  },
+  {
+    id: 3,
+    name: "pork",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.gTnpc75ixoT-dvcurE_YsQHaD-%26pid%3DApi&f=1",
+  },
+  {
+    id: 4,
+    name: "rice",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.7KY5PG2_E9mlPN2OcxcmGgHaE7%26pid%3DApi&f=1",
+  },
+  {
+    id: 5,
+    name: "salad",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.zbWkvlZszy3IHNiWljljuAHaKt%26pid%3DApi&f=1",
+  },
+];
+
 function Food({ name, picture }) {
   return (
     <div>
@@ -7,39 +40,11 @@ function Food({ name, picture }) {
   );
 }
 
-const foodILike = [
-  {
-    name: "Chicken",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.lLYnf87IJXPZqFVYyyCZDgHaDt%26pid%3DApi&f=1",
-  },
-  {
-    name: "beef",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.8SryKbw2Tuwe_9c6duQMMAHaFj%26pid%3DApi&f=1",
-  },
-  {
-    name: "pork",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.gTnpc75ixoT-dvcurE_YsQHaD-%26pid%3DApi&f=1",
-  },
-  {
-    name: "rice",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.7KY5PG2_E9mlPN2OcxcmGgHaE7%26pid%3DApi&f=1",
-  },
-  {
-    name: "salad",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.zbWkvlZszy3IHNiWljljuAHaKt%26pid%3DApi&f=1",
-  },
-];
-
 function App() {
   return (
     <div>
       {foodILike.map((dish) => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
       ))}
     </div>
   );
