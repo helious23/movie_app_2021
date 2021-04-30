d940058b95d52d7e20c786e96e23685d3d20a9c8
Max16 <max16@naver.com>
2021-04-30 17:15:55 +0900

    #6.4 Redirecting

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,19 +5,19 @@ import "./Movie.css";
 
 const Movie = ({ id, year, title, summary, poster, genres }) => {
   return (
-    <Link
-      to={{
-        pathname: "/movie-detail",
-        state: {
-          year,
-          title,
-          summary,
-          poster,
-          genres,
-        },
-      }}
-    >
-      <div className="movie">
+    <div className="movie">
+      <Link
+        to={{
+          pathname: `/movie/${id}`,
+          state: {
+            year,
+            title,
+            summary,
+            poster,
+            genres,
+          },
+        }}
+      >
         <img src={poster} alt={title} />
         <div className="movie__data">
           <h3 className="movie__title">{title}</h3>
@@ -31,8 +31,8 @@ const Movie = ({ id, year, title, summary, poster, genres }) => {
           </ul>
           <p className="movie__summary">{summary.slice(0, 180)}...</p>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
 
